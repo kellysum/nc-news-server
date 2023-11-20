6c380e65c2c7bdebfb68e533ecb5768b05b0e9d4
Kellyshek <sheksumyin@gmail.com>
2023-11-20 15:20:50 +0000

    commiting untracked changes before switching branches

---
+++ error.js
@@ -0,0 +1,21 @@
+exports.handlePsqErrors = (err, req, res, next) =>{
+    if(err.code === "22P02" || err.code === "23502") {
+       console.log(err)
+        res.status(400).send({msg: "Bad request"});
+    } else {
+        next(err)
+    }
+}
+
+exports.handleCustomErrors = (err, req, res, next) =>{
+    if(err.status){
+        res.status(err.status).send({msg: err.msg})
+    } else {
+        next(err)
+    }
+}
+
+
+exports.handle404 = (req, res) =>{
+    res.status(404).send({msg: "path not found"})
+  }
