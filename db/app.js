1caaea02c32c2384edddabf8b100d18f03dd043f
Kellyshek <sheksumyin@gmail.com>
2023-11-20 15:23:56 +0000

    amend some unrelated error handle code

--- db/app.js
+++ db/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {getAllTopics} = require("../controller/controller")
-const {handlePsqErrors, handleCustomErrors,handle404} = require("../error")
+const {handle404} = require("../error")
 
 const app = express();
 app.use(express.json())
@@ -12,8 +12,7 @@ app.get('/api/topics', getAllTopics)
 
 app.use(handle404)
 
-app.use(handlePsqErrors)
-app.use(handleCustomErrors)
+
 
 
 module.exports = app;
