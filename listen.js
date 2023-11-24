92e0ca3f76eb4a1d707d52ce75d6a73b113ff5c4
Kellyshek <sheksumyin@gmail.com>
2023-11-24 10:51:51 +0000

    changes in listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require('../be-nc-news/db/app');
+const app = require('./db/app');
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
